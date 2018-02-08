647d30d054dd3a282287e2ff0e263e572d4dd34a
RodgerOliver <timeline.mb@gmail.com>
2018-02-08 19:15:52 -0200

    ver boletim com dados da base

--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,56 @@ router.get("/home", function(req, res) {
 });
 
 router.get("/boletim", function(req, res) {
-	res.render("boletim");
+
+	var media = {};
+	// media[edfisica] => OUTPUT: 10
+	Bimestre.find({}, function(err, data) {
+		for(materia in materias) {
+			for(key in data[0]) {
+				if(materia === key) {
+					var names = data[0][key]
+					var soma = 0;
+					var recCtrl = false;
+					var result = 0;
+					var length = Number(Object.keys(names).length);
+					for(name in names) {
+						if(name === "av1" || name === "av2") {
+							if(Number(names["rec"]) > Number(names[name])) {
+								soma += Number(names["rec"]);
+							} else {
+								soma += Number(names[name]);
+							}
+						} else {
+							soma += Number(names[name]);
+						}
+					}
+
+					soma -= Number(names["rec"]);
+					result = soma/(length-1);
+					result = (Math.round(result * 2) / 2).toFixed(1);
+					media[key] = result;
+				}
+			}
+		}
+		res.render("boletim", {media: media, materias: materias});
+	});
 });
 
 router.post("/salvar", function(req, res) {
-	res.send(req.body);
+
+	var updateBimestre = {};
+
+	for(materia in materias) {
+		for(input in req.body) {
+			if(materia === input) {
+				updateBimestre[input] = req.body[input];
+			}
+		}
+	}
+
+	// Bimestre.create(updateBimestre, function(err, data) {
+	// 	res.send(data);
+	// });
 });
 
 module.exports = router;
